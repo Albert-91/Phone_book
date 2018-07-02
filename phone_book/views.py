8e16d4bc84939b3b207c899ab807d36d65a8581b
Albert-91 <albert.suralinski@gmail.com>
2018-07-02 20:47:23 +0200

    completed add person view

--- phone_book/views.py
+++ phone_book/views.py
@@ -6,6 +6,43 @@ from django.views.decorators.csrf import csrf_exempt
 
 from phone_book.models import Person
 
+form = """
+    <label>Phone number:
+        <input name='phone_number' size='10'>
+    </label>
+    <label>Phone type:
+        <select name="phone_type">
+            <option value=1>Home</option>
+            <option value=2>Business</option>
+            <option value=3>Mobile</option>
+        </select>
+    </label><br><br>"""
+form += """
+    <label>E-mail address:
+        <input name='email' size='10'>
+    </label>
+    <label>Phone type:
+        <select name="email_type">
+            <option value=1>Home</option>
+            <option value=2>Business</option>
+            <option value=3>Mobile</option>
+        </select>
+    </label><br><br>"""
+form += """
+    <label>Street:
+        <input name='street' size='10'>
+    </label>
+    <label>House number:
+        <input name='house_number' size='5'>
+    </label>
+    <label>Apartment number:
+        <input name='apartment_number' size='5'>
+    </label>
+    <label>City:
+        <input name='city' size='10'>
+    </label><br><br>
+"""
+
 
 def decor_warp_html(form):
     def wrap_html(*args, **kwargs):
@@ -54,49 +91,14 @@ class AddPerson(View):
         form += """
             <label> Name:
                 <input name='name' size='10'>
-            </label>
+            </label><br><br>
             <label> Surname:
                 <input name='surname' size='10'>
-            </label>
-            <label> Description:
-                <input type="text" name='description'>
-            </label><br><br>"""
-        form += """
-            <label>Phone number:
-                <input name='phone_number' size='10'>
-            </label>
-            <label>Phone type:
-                <select name="phone_type">
-                    <option value=1>Home</option>
-                    <option value=2>Business</option>
-                    <option value=3>Mobile</option>
-                </select>
-            </label><br><br>"""
-        form += """
-            <label>E-mail address:
-                <input name='email' size='10'>
-            </label>
-            <label>Phone type:
-                <select name="email_type">
-                    <option value=1>Home</option>
-                    <option value=2>Business</option>
-                    <option value=3>Mobile</option>
-                </select>
-            </label><br><br>"""
-        form += """
-            <label>Street:
-                <input name='street' size='10'>
-            </label>
-            <label>House number:
-                <input name='house_number' size='5'>
-            </label>
-            <label>Apartment number:
-                <input name='apartment_number' size='5'>
-            </label>
-            <label>City:
-                <input name='city' size='10'>
             </label><br><br>
-        """
+            <label> Description:
+                <textarea name='description' cols='20' rows='2' placeholder='Describe here'></textarea>
+            </label><br><br>"""
+
         form += "<input type='submit' value='Dodaj osobę'></form>"
         return form
 
@@ -105,7 +107,12 @@ class AddPerson(View):
         name = request.POST.get('name')
         surname = request.POST.get('surname')
         description = request.POST.get('description')
-        Person.objects.create(name=name, surname=surname, description=description)
+        if name and surname:
+            Person.objects.create(name=name, surname=surname, description=description)
+            response = '{} {} was added!'.format(name, surname)
+        else:
+            response = 'Not enough data'
+        return response
 
 
 @method_decorator(csrf_exempt, name='dispatch')
