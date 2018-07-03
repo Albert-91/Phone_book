521911f34cc806581dc8b49fa12cc75051fd1712
Albert-91 <albert.suralinski@gmail.com>
2018-07-03 21:15:14 +0200

    completed showing all view and deleting from db

--- phone_book/views.py
+++ phone_book/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
-from phone_book.models import Person
+from phone_book.models import Person, Email, Phone, Address
 
 form = """
     <label>Phone number:
@@ -58,27 +59,80 @@ def decor_warp_html(form):
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ShowAll(View):
+    @decor_warp_html
     def get(self, request):
-        pass
-
-    def post(self, request):
-        pass
+        table = """
+            <table border=1>
+                <tr>
+                    <td align="center">Surname and name</td>
+                    <td align="center">Options</td>
+                </tr>
+            """
+        persons = Person.objects.order_by('surname')
+        for person in persons:
+            table += """
+                <tr>
+                    <td align="center"><a href='/person/{0}' style="color:black">{1} {2}</a></td>
+                    <td align="center"><a href='/delete/{0}' style="color:red">delete</a>
+                        <a href='/modify/{0}' style="color:green"> modify</a></td>
+                </tr>
+                """.format(person.id, person.surname, person.name)
+        table += "</table><br>"
+        table += """<form><button formaction="/new/">Add new person</button></form>"""
+        return table
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ShowDetail(View):
-    def get(self, request):
-        pass
+    @decor_warp_html
+    def get(self, request, id):
+        person = Person.objects.get(id=id)
+        table = """
+            <table border=1>
+                <tr>
+                    <td></td>
+                    <td align="center">Data</td>
+                </tr>
+                <tr>
+                    <td>Name</td>
+                    <td align="center">{}</td>
+                </tr>
+                <tr>
+                    <td>Surname</td>
+                    <td align="center">{}</td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td align="center">{}</td>
+                </tr>
+                
+        """.format(person.name, person.surname, person.description)
 
-    def post(self, request):
-        pass
+        emails = Email.objects.filter(person_id=id)
+        phones = Phone.objects.filter(person_id=id)
+        addresses = Address.objects.filter(person_id=id)
+        # if emails or phones or addresses:
+        #     for email in emails:
+        #         table += """
+        #             <tr>
+        #                 <td>Email</td>
+        #                 <td align="center">{}</td>
+        #             </tr>
+        #         """
+
+        # groups = Address.objects.filter(person_id=id)
+        table += "</table><br>"
+        table += """<form><button formaction="/modify/{}">Modify person</button></form>""".format(id)
+        return table
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class ModifyPerson(View):
-    def get(self, request):
-        pass
+    @decor_warp_html
+    def get(self, request, id):
+        return "modified"
 
+    @decor_warp_html
     def post(self, request):
         pass
 
@@ -102,14 +156,14 @@ class AddPerson(View):
         form += "<input type='submit' value='Dodaj osobę'></form>"
         return form
 
-    @decor_warp_html
     def post(self, request):
         name = request.POST.get('name')
         surname = request.POST.get('surname')
         description = request.POST.get('description')
         if name and surname:
             Person.objects.create(name=name, surname=surname, description=description)
-            response = '{} {} was added!'.format(name, surname)
+            last_id = Person.objects.order_by('-id')[0]
+            response = HttpResponseRedirect('/person/{}'.format(last_id.id))
         else:
             response = 'Not enough data'
         return response
@@ -117,8 +171,11 @@ class AddPerson(View):
 
 @method_decorator(csrf_exempt, name='dispatch')
 class DeletePerson(View):
-    def get(self, request):
-        pass
+    @decor_warp_html
+    def get(self, request, id):
+        person = Person.objects.get(id=id)
+        person.delete()
+        answer = "{} {} was deleted from database".format(person.name, person.surname)
+        return answer
 
-    def post(self, request):
-        pass
+
